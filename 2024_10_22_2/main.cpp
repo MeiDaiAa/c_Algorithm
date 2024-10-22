c9d5978c7d15abb816311c864a4651e04b111d48
MeiDaiAa <114286638+MeiDaiAa@users.noreply.github.com>
2024-10-22 13:21:43 +0800

    904.水果成篮

---
+++ 2024_10_22_2/main.cpp
@@ -0,0 +1,10 @@
+#include "totalFruit.cpp"
+
+int main(){
+    Solution solution;
+
+    vector<int> fruits = {0,1,2,2};
+    cout << "[" << solution.totalFruit(fruits) << "]";
+
+    return EXIT_SUCCESS;
+}
