e058bf1044b14a862e243ca2f2f40106dc46b29b
MeiDaiAa <114286638+MeiDaiAa@users.noreply.github.com>
2024-11-17 21:31:35 +0800

    周赛第 424 场

--- 周赛/424 场/Q1. 使数组元素等于零/countValidSelections.cpp
+++ 周赛/424 场/Q1. 使数组元素等于零/countValidSelections.cpp
@@ -2,9 +2,28 @@
 #include <iostream>
 using namespace std;
 
-//方法1：模拟
+
+/*
+给你一个整数数组 nums 。
+
+开始时，选择一个满足 nums[curr] == 0 的起始位置 curr ，并选择一个移动 方向 ：向左或者向右。
+
+此后，你需要重复下面的过程：
+
+如果 curr 超过范围 [0, n - 1] ，过程结束。
+如果 nums[curr] == 0 ，沿当前方向继续移动：如果向右移，则 递增 curr ；如果向左移，则 递减 curr 。
+如果 nums[curr] > 0:
+将 nums[curr] 减 1 。
+反转 移动方向（向左变向右，反之亦然）。
+沿新方向移动一步。
+如果在结束整个过程后，nums 中的所有元素都变为 0 ，则认为选出的初始位置和移动方向 有效 。
+
+返回可能的有效选择方案数目。
+*/
+
 class Solution {
 public:
+    //方法1：模拟
     vector<int> differ;
     int res = 0;
     int countValidSelections(vector<int>& nums) {
